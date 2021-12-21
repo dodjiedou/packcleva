9e307a345c6101b07df7f2cbfb9bed292cf0a148
fl4sh <fl4sh@samajean88@gmail.com>
2021-12-21 17:07:49 +0000

    modifié :         src/Controller/BeneficiaireController.php 	supprimé :        templates/dashboards/beneficiaire/afficherbeneficiaire.html.twig 	supprimé :        templates/dashboards/beneficiaire/creerbeneficiaire.html.twig 	supprimé :        templates/dashboards/beneficiaire/delete_beneficiaire.html.twig 	supprimé :        templates/dashboards/beneficiaire/edit_beneficiaire.html.twig 	modifié :         templates/dashboards/beneficiaire/index.html.twig 	supprimé :        templates/dashboards/beneficiaire/list_beneficiaire.html.twig
     Fichiers non suivis:
    	templates/dashboards/beneficiaire/_delete_form.html.twig
    	templates/dashboards/beneficiaire/_form.html.twig
    	templates/dashboards/beneficiaire/edit.html.twig
    	templates/dashboards/beneficiaire/new.html.twig
    	templates/dashboards/beneficiaire/show.html.twig

--- src/Controller/BeneficiaireController.php
+++ src/Controller/BeneficiaireController.php
@@ -13,81 +13,184 @@ use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-
+/**
+ * @Route("/dashboard/beneficiaire")
+ */
 class BeneficiaireController extends AbstractController
 {
 
-    /*
-        /list_beneficiaire
-        /creerbeneficiaire
-        /showbeneficiaire/{num}
-        /edit_beneficiaire
-        /delete_beneficiaire/{num}
-    */
-
-
-    #[Route('/list_beneficiaire', name: 'list_beneficiaire')]
-    public function index(): Response
+    /**
+     * @Route("/", name="beneficiaire_index", methods={"GET"})
+     */
+    public function index(BeneficiaireRepository $beneficiaireRepository): Response
     {
-        $beneficiaires = $this->getDoctrine()
-            ->getRepository(Beneficiaire::class)->findAll();
-        return $this->render('dashboards/beneficiaire/list_beneficiaire.html.twig',[
-            'beneficiaires' => $beneficiaires]);
-        
+        return $this->render('dashboards/beneficiaire/index.html.twig', [
+            'beneficiaires' => $beneficiaireRepository->findAll(),
+        ]);
     }
 
-    #[Route('/creerbeneficiaire', name: 'creerbeneficiaire')]
-    public function new(Request $request): Response
+    /**
+     * @Route("/new", name="beneficiaire_new", methods={"GET","POST"})
+     */
+    public function new(
+        Request $request,
+        BeneficiaireRepository $beneficiaireRepository
+    ): Response
     {
-       $beneficiaire = new Beneficiaire();
-        $personne = new Personne();
-        $entityManager = $this->getDoctrine()->getManager();
-
-        $form = $this->createForm(BeneficiaireFormType::class, $beneficiaire);
-
+        $beneficiaire = new Beneficiaire();
+        $beneficiaire->setAuthor($this->getUser());
+        $form = $this->createForm(BeneficiaireType::class, $beneficiaire);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-
-            $beneficiaire = $form->getData();
+            $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($beneficiaire);
             $entityManager->flush();
-            return $this->redirectToRoute('creerbeneficiaire');
+
+            $this->addFlash("success","Bénéficiaire ".$beneficiaire->getPrenom()." ".$beneficiaire->getNom()." créé(e) avec succès !");
+
+            return $this->redirectToRoute(
+                'beneficiaire_index', 
+                [
+                    'beneficiaires' => $beneficiaireRepository->findAll()
+                ], 
+                Response::HTTP_SEE_OTHER
+            );
         }
 
-        return $this->render('dashboards/beneficiaire/creerbeneficiaire.html.twig', [
-            'form' => $form->createView()]);
-         
+        return $this->renderForm('dashboards/beneficiaire/new.html.twig', [
+            'beneficiaire' => $beneficiaire,
+            'form' => $form,
+        ]);
     }
 
-    #[Route('/showbeneficiaire/{num}', name: 'showbeneficiaire')]
-    public function show($num): Response
+    /**
+     * @Route("/show/{slug}", name="beneficiaire_show", methods={"GET"})
+     */
+    public function show(Beneficiaire $beneficiaire): Response
     {
-        $benef = $this->getDoctrine()
-            ->getRepository(Beneficiaire::class)->findOneBy(["numero"=>$num]);
-            //$benef = $this->getDoctrine()
-            //->getRepository(Beneficiaire::class)->find($id);
-            
-        return $this->render('dashboards/beneficiaire/afficherbeneficiaire.html.twig', compact('benef'));
+        return $this->render('dashboards/beneficiaire/show.html.twig', [
+            'beneficiaire' => $beneficiaire,
+            'slug' => $beneficiaire->getSlug(),
+        ]);
     }
 
-    #[Route('/edit_beneficiaire', name: 'edit_beneficiaire')]
-    public function edit(): Response
+    /**
+     * @Route("/edit/{slug}", name="beneficiaire_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Beneficiaire $beneficiaire): Response
     {
-        return $this->render('dashboards/beneficiaire/edit_beneficiaire.html.twig');
+        $form = $this->createForm(BeneficiaireType::class, $beneficiaire);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            $this->addFlash("success","Bénéficiaire ".$beneficiaire->getPrenom()." ".$beneficiaire->getNom()." modifié(e) avec succès !");
+
+            return $this->redirectToRoute('beneficiaire_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('dashboards/beneficiaire/edit.html.twig', [
+            'beneficiaire' => $beneficiaire,
+            'form' => $form,
+        ]);
     }
 
-    #[Route('/delete_beneficiaire/{num}', name: 'delete_beneficiaire')]
-    public function delete($num): Response
+    /**
+     * @Route("/delete/{slug}", name="beneficiaire_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Beneficiaire $beneficiaire): Response
     {
-        $entityManager = $this->getDoctrine()->getManager();
-        $benef = $this->getDoctrine()
-            ->getRepository(Beneficiaire::class)->findOneBy(["numero"=>$num]);
-        $entityManager->remove($benef);
-        $entityManager->flush();
-        $beneficiaires = $this->getDoctrine()
-            ->getRepository(Beneficiaire::class)->findAll();
-        return $this->render('dashboards/beneficiaire/list_beneficiaire.html.twig',[
-            'beneficiaires' => $beneficiaires]);
+        if ($this->isCsrfTokenValid('delete'.$beneficiaire->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($beneficiaire);
+            $entityManager->flush();
+            $this->addFlash("success","Bénéficiaire ".$beneficiaire->getPrenom()." ".$beneficiaire->getNom()." supprimé(e) avec succès !");
+        }
+
+        return $this->redirectToRoute('beneficiaire_index', [], Response::HTTP_SEE_OTHER);
     }
+
 }
+
+
+// class BeneficiaireController extends AbstractController
+// {
+
+//     /*
+//         /list_beneficiaire
+//         /creerbeneficiaire
+//         /showbeneficiaire/{num}
+//         /edit_beneficiaire
+//         /delete_beneficiaire/{num}
+//     */
+
+
+//     #[Route('/list_beneficiaire', name: 'list_beneficiaire')]
+//     public function index(): Response
+//     {
+//         $beneficiaires = $this->getDoctrine()
+//             ->getRepository(Beneficiaire::class)->findAll();
+//         return $this->render('dashboards/beneficiaire/list_beneficiaire.html.twig',[
+//             'beneficiaires' => $beneficiaires]);
+        
+//     }
+
+//     #[Route('/creerbeneficiaire', name: 'creerbeneficiaire')]
+//     public function new(Request $request): Response
+//     {
+//        $beneficiaire = new Beneficiaire();
+//         $personne = new Personne();
+//         $entityManager = $this->getDoctrine()->getManager();
+
+//         $form = $this->createForm(BeneficiaireFormType::class, $beneficiaire);
+
+//         $form->handleRequest($request);
+
+//         if ($form->isSubmitted() && $form->isValid()) {
+
+//             $beneficiaire = $form->getData();
+//             $entityManager->persist($beneficiaire);
+//             $entityManager->flush();
+//             return $this->redirectToRoute('creerbeneficiaire');
+//         }
+
+//         return $this->render('dashboards/beneficiaire/creerbeneficiaire.html.twig', [
+//             'form' => $form->createView()]);
+         
+//     }
+
+//     #[Route('/showbeneficiaire/{num}', name: 'showbeneficiaire')]
+//     public function show($num): Response
+//     {
+//         $benef = $this->getDoctrine()
+//             ->getRepository(Beneficiaire::class)->findOneBy(["numero"=>$num]);
+//             //$benef = $this->getDoctrine()
+//             //->getRepository(Beneficiaire::class)->find($id);
+            
+//         return $this->render('dashboards/beneficiaire/afficherbeneficiaire.html.twig', compact('benef'));
+//     }
+
+//     #[Route('/edit_beneficiaire', name: 'edit_beneficiaire')]
+//     public function edit(): Response
+//     {
+//         return $this->render('dashboards/beneficiaire/edit_beneficiaire.html.twig');
+//     }
+
+//     #[Route('/delete_beneficiaire/{num}', name: 'delete_beneficiaire')]
+//     public function delete($num): Response
+//     {
+//         $entityManager = $this->getDoctrine()->getManager();
+//         $benef = $this->getDoctrine()
+//             ->getRepository(Beneficiaire::class)->findOneBy(["numero"=>$num]);
+//         $entityManager->remove($benef);
+//         $entityManager->flush();
+//         $beneficiaires = $this->getDoctrine()
+//             ->getRepository(Beneficiaire::class)->findAll();
+//         return $this->render('dashboards/beneficiaire/list_beneficiaire.html.twig',[
+//             'beneficiaires' => $beneficiaires]);
+//     }
+// }
+
+
